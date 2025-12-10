de16d7b5e19af007eb53be287e0402fef20a9673
Jay <t4nyou2@gmail.com>
2025-12-10 10:27:12 +0800

    update the BMI to make it work using php

--- Pages/BMI-Calculator.php
+++ Pages/BMI-Calculator.php
@@ -1,3 +1,24 @@
+
+<?php
+
+$result = "BMI Calculator";
+if(isset($_POST['submit'])){
+  $height = $_POST["height"] / 100;
+  $weight = $_POST["weight"] ;
+  $total = $weight / ($height * $height);
+  $total =round($total ,2);
+  if($total <= 18.5){
+    $result = "your BMI is $total, you are underweight";
+  }elseif($total >= 18.6 && $total <= 24.9 ){
+    $result = "your BMI is $total, you are Normal";
+  }elseif($total >= 25 && $total <= 29.9){
+    $result = "your BMI is $total, you are overweight";
+  }else{
+    $result = "your BMI is $total, you are obbese";
+  }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,26 +29,20 @@
 </head>
 <body style="background-image: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), 
  url(/Age-Course-BMI_Calculator/Img/bmi.jpg);background-size: cover; "> 
- 
- <h1 id="dashboardText">Welcome To BMI Calculator</h1>
+
+ <h1 id="dashboardText"><?php  echo $result; ?></h1>
   
  <div class = "form-parent-container" >
   <div class="form-container">
     <h1>BMI Calculator</h1>
     <form action="BMI-Calculator.php" method="post">
       <label >Enter Height And Width</label><br>
-      <input type="text" class="input" placeholder="Height" require><br>
-      <input type="text" class="input" placeholder="Weight" require><br>
+      <input type="text" class="input" name = "height" placeholder="Height(CM)" require><br>
+      <input type="text" class="input" name = "weight" placeholder="Weight(WG)" require><br>
       <button type = "submit" name = "submit" >Calculate</button>
-
-
-
-
     </form>
-
-
-
   </div>
   </div>
 </body>
 </html>
+
